3668367995f9826ca8382ec9de26a4ef61768f7d
Talha Satır <imnever0729@gmail.com>
2023-01-11 03:53:10 +0300

    - jquery kütüphanesi projeye dahil edildi - apiden get methodu alma başarılı bir şekilde gerçekleştirildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var $:any //jquery kütüphansini ilgili componente bağlı sayfalarda kullanabilmek için dolar işaretini burada build ediyiyorum. 
+//jquery'in başarılı bir şekilde projeye dahil edildiğini anlayabiliyorum jqueryin doları ile.
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ETicaretClient';
 }
+/*
+$.get("https://localhost:7071/api/products", data=>{console.log(data)}) //api uygulammızdan istekde bulunuyoruz = cross politikalarını test etmek için == apide program.cs de cross politikalarını herkeze veya bu uygulamaya izin vermediğimde hata aldım. izin verdikten sonra apiden veri çekebildim buraya.
+*/
+$.get("https://localhost:7030/api/products")
+
+/* //jquery testini yaptık.
+$(document).ready(()=>{ //sayfa açıldığında jquery ile alert basıyoruz.
+  alert("asd")
+})
+*/
