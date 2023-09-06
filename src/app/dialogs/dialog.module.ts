6c92df40c0601a53d95ca5dc4d3b38b7c045b1ac
Talha <talha.satir@vinya.com.tr>
2023-09-06 18:04:12 +0300

    feat - Endpoint'lerle Rollerin İlişkilendirilmesi

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -15,6 +15,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { CompleteOrderDialogComponent } from './complete-order-dialog/complete-order-dialog.component';
 import { AuthorizeMenuDialogComponent } from './authorize-menu-dialog/authorize-menu-dialog.component';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,7 @@ import {MatBadgeModule} from '@angular/material/badge';
   ],
   imports: [
     CommonModule,
-    MatDialogModule,MatButtonModule,MatCardModule, MatTableModule, MatToolbarModule, MatBadgeModule,
+    MatDialogModule,MatButtonModule,MatCardModule, MatTableModule, MatToolbarModule, MatBadgeModule, MatListModule,
     FileUploadModule,
     FormsModule
   ]
